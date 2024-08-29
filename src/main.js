606f42e71f88eede7ad98f897ef70ef6bd6c6a61
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2024-08-29 15:41:23 -0500

    chore: initial commit.

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+//los componentes encapsulan comportamientos del programa--idicando
+//#app root coiside con el app de index
+createApp(App).mount('#app')//symbol page aplication

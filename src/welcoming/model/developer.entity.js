ded5d562983d971257c439aba5a196939f712b74
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2024-08-29 18:27:20 -0500

    docs: added developer entity documentation comments.

--- src/welcoming/model/developer.entity.js
+++ src/welcoming/model/developer.entity.js
@@ -1,6 +1,19 @@
+/**
+ * Developer entity
+ * @description
+ * This class represents a developer entity.
+ * It has a first name and a last name.
+ * */
+
 export class Developer{
     firstName;
     lastName;
+
+    /**
+     * Constructor
+     * @param firstName - The first name of the developer.
+     * @param lastName - The last name of the developer.
+     */
 
     constructor(firstName, lastName)
     {
@@ -8,6 +21,10 @@ export class Developer{
         this.lastName=lastName;
     }
 
+    /**
+     * Get the full name of the developer.
+     * @returns {string|string} The full name of the developer.
+     */
     get fullName(){
         return this.firstName == null || this.lastName == null ? "Unknown":
             `${this.firstName} ${this.lastName}`.trim();

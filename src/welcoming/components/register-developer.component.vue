d207e241ec91a93c57d890c5913f5d85b52c9c0b
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2024-08-29 16:16:17 -0500

    feat(welcoming): added register developer components.

---
+++ src/welcoming/components/register-developer.component.vue
@@ -0,0 +1,48 @@
+<script>
+export default {
+  name: "register-developer.component",
+  data(){
+    return {
+      firstName: '',
+      lastName: ''
+    }
+  },
+  methods: {
+    onSubmit(){
+      console.log('Registering Developer');
+      this.$emit('developer-registered', {
+        firstName: this.firstName,
+        lastName: this.lastName
+      });
+    }
+
+  }
+}
+</script>
+//convinacion de codigo HTML y vue
+<template>
+  <div>
+    <h1>New Developer</h1>
+    <div>
+      <form @submit.prevent="onSubmit"><!--@submit.prevent es para que no se recargue la pagina -->
+        <div>
+          <label for="firstName">First Name</label>
+          <input type="text" id="firstName" v-model="firstName" required/>,
+          <!--v-model hace debe guardarlo en el properties-->
+        </div>
+        <div>
+          <label for="lastName">Last Name</label>
+          <input type="text" id="lastName" v-model="lastName" required/>
+        </div>
+        <div>
+          <button type="submit">Register</button>
+        </div>
+      </form>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
